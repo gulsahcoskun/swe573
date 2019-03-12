7d8890c9919bfacc02ffd1bf40218f7514648edf
gulsah <gulsahcoskunn@gmail.com>
2019-03-12 13:25:39 +0300

    initial commit

---
+++ src/main/java/com/boun/glearn/GLearnApplication.java
@@ -0,0 +1,13 @@
+package com.boun.glearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GLearnApplication.class, args);
+    }
+
+}
